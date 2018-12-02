e14f543cc3ae041bb035f04d596858815fd6e25e
guaychou <crossmajor99@gmail.com>
2018-12-02 11:36:12 +0700

    modified service insert

--- service_insert.php
+++ service_insert.php
@@ -1,6 +1,14 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-require_once ('koneksi.php');
+define('HOST','localhost');
+define('USER','u420353749_ppk');
+//sesuaikan nama user
+define('PASS','rahasia');
+//sesuaiakan nama password
+define('DB','u420353749_tdl');
+//sesuaiakan naman database
+$conn = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+date_default_timezone_set("Asia/Jakarta");
 if(isset($_POST['nama'])&& isset($_POST['email'])&& isset($_POST['password'])){
     //$QUERY = MYSQLI_QUERY($conn,"SELECT * FROM player where nama like '%".$_GET['nama']."%'");
     echo $_POST['nama']."==>".$_POST['email']."==>" .$_POST['password'];
