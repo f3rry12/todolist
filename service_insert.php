a898eb397909e23c3d1b8442ea6ee58064f99f48
guaychou <crossmajor99@gmail.com>
2018-12-02 12:22:13 +0700

    service_insert done

--- service_insert.php
+++ service_insert.php
@@ -1,20 +1,24 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-define('HOST','localhost');
-define('USER','u420353749_ppk');
-//sesuaikan nama user
-define('PASS','rahasia');
-//sesuaiakan nama password
-define('DB','u420353749_tdl');
-//sesuaiakan naman database
-$conn = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+$dbhost = 'localhost';
+$dbname = 'u420353749_tdl';
+$dbusername = 'u420353749_ppk';
+$dbpassword = 'rahasia';
+//$conn = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+$link = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
 date_default_timezone_set("Asia/Jakarta");
 if(isset($_POST['nama'])&& isset($_POST['email'])&& isset($_POST['password'])){
     $nama=$_POST['nama'];
     $email=$_POST['email'];
     $password=$_POST['password'];
     try{
-        $QUERY = MYSQLI_QUERY($conn,"INSERT INTO USER values ($nama,$email,$password");
+        $statement = $link->prepare('INSERT INTO user (nama,email,password)VALUES (:nama, :email, :password)');
+        $statement->execute([
+            'nama' => "$nama",
+            'email' => "$email",
+            'password' => "$password",
+        ]);
+        echo "Success";
     }
     catch(Exception $e){
         $message=$e->getMessage();
