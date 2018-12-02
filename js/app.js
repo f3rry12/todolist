f7e81146738468d5f217b4e5b9685cd8b3141ce0
zakwandhiya <zakwandhiya@gmail.com>
2018-12-02 13:10:39 +0700

    update login

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+var gEmail;
+var gNama;
+
 var Application = {
 	initApplication: function () {
 		$(document).on('click','#detail-player',function(){
@@ -13,17 +16,41 @@ var Application = {
 			var pass = document.getElementById('txt-password').value;
 			var repass = document.getElementById('txt-password-confirm').value;
 
-			if(name === "" || email === "" || pass === "" || repass === "")
-				alert("isi semua field terlebih dahulu")
+			if(name === "" || email === "" || pass === "" || repass === ""){
+				document.getElementById("snackbar").innerHTML = "isi semua field terlebih dahulu";
+				var x = document.getElementById("snackbar");
+				x.className = "show";
+				setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+			}
 			else if( pass === repass)
 				Application.register(name , email , pass);
-			else alert("password dan repassword tidak sama");
+			else {
+				document.getElementById("snackbar").innerHTML = "password dan repassword tidak sama";
+				var x = document.getElementById("snackbar");
+				x.className = "show";
+				setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+			}
+		})
+
+		$(document).on('click','#btn-submit-login',function(){
+			var id_player =$(this).data('id_player');
+			var email = document.getElementById('txt-email').value;
+			var pass = document.getElementById('txt-password').value;
+
+			if(email === "" || pass === ""){
+				document.getElementById("snackbar").innerHTML = "isi semua field terlebih dahulu";
+				var x = document.getElementById("snackbar");
+				x.className = "show";
+				setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+			}
+			else Application.login(email , pass);
 		})
 	},
 
 	register : function(mUsername , mEmail , mPassword) {
+		console.log(mUsername + mEmail + mPassword )
     $.ajax({
-      url: 'http://amamipro.site/service_insert.php',
+      url: 'http://amamipro.site/service_register.php',
       type: 'POST',
       data: { nama : mUsername, email : mEmail , password : mPassword} ,
       beforeSend : function() {
@@ -34,11 +61,17 @@ var Application = {
       },
       success : function(dataObject) {
         console.log(dataObject)
-        if(dataObject.status === true){
+        if(dataObject == "sukses"){
+					console.log("true")
           window.location.replace("./index.html");
+				} else {
+					
+					document.getElementById("snackbar").innerHTML = "i";
+					var x = document.getElementById("snackbar");
+					x.className = "show";
+					setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
 				}
 				
-				if(dataObject === "error bang"){}
       },
       complete : function() {
         $.mobile.loading('hide');
@@ -48,7 +81,7 @@ var Application = {
 
 	login : function(mEmail , mPassword) {
     $.ajax({
-      url: 'https://riwayat-pasien.ngopi.men/patient_service.php',
+      url: 'http://amamipro.site/service_login.php',
       type: 'POST',
       data: { email : mEmail, password : mPassword} ,
       beforeSend : function() {
@@ -59,7 +92,8 @@ var Application = {
       },
       success : function(dataObject) {
         console.log(dataObject)
-        if(dataObject.status === true){
+				console.log(dataObject.value["nama"])
+        if(dataObject["nama"] != null){
           window.location.replace("./home.html");
         }
       },
@@ -287,5 +321,13 @@ var Application = {
 			complete:function(){
 				$.mobile.loading('hide');
 		}});
+
+		// showSnackBar : function(text){
+		// 	document.getElementById("snackbar").innerHTML = "isi semua field terlebih dahulu";
+		// 	var x = document.getElementById("snackbar");
+		// 	x.className = "show";
+		// 	setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+		// }
 	}
+
 };
