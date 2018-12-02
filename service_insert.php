03412fd70f415e235b5ee30ffca0d3f8ab0ea26c
guaychou <crossmajor99@gmail.com>
2018-12-02 11:47:28 +0700

    update service_insert.php

--- service_insert.php
+++ service_insert.php
@@ -10,8 +10,17 @@ define('DB','u420353749_tdl');
 $conn = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 date_default_timezone_set("Asia/Jakarta");
 if(isset($_POST['nama'])&& isset($_POST['email'])&& isset($_POST['password'])){
-    //$QUERY = MYSQLI_QUERY($conn,"SELECT * FROM player where nama like '%".$_GET['nama']."%'");
-    echo $_POST['nama']."==>".$_POST['email']."==>" .$_POST['password'];
+    $nama=$_POST['nama'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    try{
+        $QUERY = MYSQLI_QUERY($conn,"INSERT INTO USER values ($nama,$email,$password");
+    }
+    catch(Exception $e){
+        $message=$e->getMessage();
+        echo JSON_ENCODE($message);
+    }
+   // echo $_POST['nama']."==>".$_POST['email']."==>" .$_POST['password'];
 }else{
     echo "error bang";
 }
