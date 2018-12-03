1b3b33ea33a2adc123784c912dd0ca0dee0ecd1a
guaychou <crossmajor99@gmail.com>
2018-12-03 18:22:47 +0700

    modified service_login

--- service_login.php
+++ service_login.php
@@ -4,7 +4,6 @@ $dbhost = 'localhost';
 $dbname = 'u420353749_tdl';
 $dbusername = 'u420353749_ppk';
 $dbpassword = 'rahasia';
-//$conn = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
 $link = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbusername, $dbpassword);
 date_default_timezone_set("Asia/Jakarta");
 if(isset($_POST['email'])&& isset($_POST['password'])){
@@ -12,20 +11,26 @@ if(isset($_POST['email'])&& isset($_POST['password'])){
     $email=$_POST['email'];
     $password=$_POST['password'];
     try{
-        $statement = $link->prepare('SELECT nama, email from user where email=:email password=:password');
+        $statement = $link->prepare('SELECT nama,email from user where email= :email and password= :password');
         $statement->execute([
             'email' => "$email",
             'password' => "$password",
         ]);
         if($statement->rowCount()==1){
-            echo "Login Success";
+            
+            $user=$statement->fetch();
+            $data=array();
+            $data['nama']=$user['nama'];
+            $data['email']=$user['email'];
+            echo json_encode($data);
         }
+        
+        
     }
     catch(Exception $e){
         $message=$e->getMessage();
         echo JSON_ENCODE($message);
     }
-   // echo $_POST['nama']."==>".$_POST['email']."==>" .$_POST['password'];
 }else{
     echo "error bang";
 }
