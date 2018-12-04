6971eedf745356f51d4ab31d5e22bdb9c83343b0
zakwandhiya <zakwandhiya@gmail.com>
2018-12-04 09:51:26 +0700

    update login

--- js/app.js
+++ js/app.js
@@ -3,54 +3,60 @@ var gNama;
 
 var Application = {
   initApplication: function() {
-		Application.initShowPlayer();
+    // Application.initShowPlayer();
+    gEmail = localStorage['gEmail'] || null;
+    gNama = localStorage['gNama'] || null;   
+
+    console.log(gEmail)
+    console.log(gNama)
+
+    $("#wellcome").html(" Selamat datang," + gEmail);
 
 
+  //register
+  $(document).on('click', '#btn-submit', function() {
+    var name = document.getElementById('txt-name').value;
+    var email = document.getElementById('txt-email').value;
+    var pass = document.getElementById('txt-password').value;
+    var repass = document.getElementById('txt-password-confirm').value;
 
-//register
-$(document).on('click', '#btn-submit', function() {
-  var name = document.getElementById('txt-name').value;
-  var email = document.getElementById('txt-email').value;
-  var pass = document.getElementById('txt-password').value;
-  var repass = document.getElementById('txt-password-confirm').value;
+    if (name === "" || email === "" || pass === "" || repass === "") {
+      document.getElementById("snackbar").innerHTML = "isi semua field terlebih dahulu";
+      var x = document.getElementById("snackbar");
+      x.className = "show";
+      setTimeout(function() {
+        x.className = x.className.replace("show", "");
+      }, 3000);
+    } else if (pass === repass)
+      Application.register(name, email, pass);
+    else {
+      document.getElementById("snackbar").innerHTML = "password dan repassword tidak sama";
+      var x = document.getElementById("snackbar");
+      x.className = "show";
+      setTimeout(function() {
+        x.className = x.className.replace("show", "");
+      }, 3000);
+    }
+  })
 
-  if (name === "" || email === "" || pass === "" || repass === "") {
-    document.getElementById("snackbar").innerHTML = "isi semua field terlebih dahulu";
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    setTimeout(function() {
-      x.className = x.className.replace("show", "");
-    }, 3000);
-  } else if (pass === repass)
-    Application.register(name, email, pass);
-  else {
-    document.getElementById("snackbar").innerHTML = "password dan repassword tidak sama";
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    setTimeout(function() {
-      x.className = x.className.replace("show", "");
-    }, 3000);
-  }
-})
+  //login
+  $(document).on('click', '#btn-submit-login', function() {
+    var email = document.getElementById('txt-email').value;
+    var pass = document.getElementById('txt-password').value;
 
-//login
-$(document).on('click', '#btn-submit-login', function() {
-  var email = document.getElementById('txt-email').value;
-  var pass = document.getElementById('txt-password').value;
+    if (email === "" || pass === "") {
+      document.getElementById("snackbar").innerHTML = "isi semua field terlebih dahulu";
+      var x = document.getElementById("snackbar");
+      x.className = "show";
+      setTimeout(function() {
+        x.className = x.className.replace("show", "");
+      }, 3000);
+    } else Application.login(email, pass);
+  })
+  },
 
-  if (email === "" || pass === "") {
-    document.getElementById("snackbar").innerHTML = "isi semua field terlebih dahulu";
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    setTimeout(function() {
-      x.className = x.className.replace("show", "");
-    }, 3000);
-  } else Application.login(email, pass);
-})
-},
-
-//coba-coba
-initShowPlayer: function () {
+  //coba-coba
+  initShowPlayer: function () {
 		$.ajax({
 			url : 'http://amamipro.site/service_coba.php',
 			type: 'get',
@@ -78,47 +84,47 @@ initShowPlayer: function () {
 		});
 
 	},
-//register
-register: function(mUsername, mEmail, mPassword) {
-    console.log(mUsername + mEmail + mPassword)
-    $.ajax({
-      url: 'http://amamipro.site/service_register.php',
-      type: 'POST',
-      data: {
-        nama: mUsername,
-        email: mEmail,
-        password: mPassword
-      },
-      beforeSend: function() {
-        $.mobile.loading('show', {
-          text: 'Loading',
-          textVisible: true
-        })
-      },
-      success: function(dataObject) {
-        console.log(dataObject)
-        if (dataObject == "sukses") {
-          console.log("true")
-          window.location.replace("./index.html");
-        } else {
+  //register
+  register: function(mUsername, mEmail, mPassword) {
+      console.log(mUsername + mEmail + mPassword)
+      $.ajax({
+        url: 'http://amamipro.site/service_register.php',
+        type: 'POST',
+        data: {
+          nama: mUsername,
+          email: mEmail,
+          password: mPassword
+        },
+        beforeSend: function() {
+          $.mobile.loading('show', {
+            text: 'Loading',
+            textVisible: true
+          })
+        },
+        success: function(dataObject) {
+          console.log(dataObject)
+          if (dataObject == "sukses") {
+            console.log("true")
+            window.location.replace("./index.html");
+          } else {
 
-          document.getElementById("snackbar").innerHTML = "i";
-          var x = document.getElementById("snackbar");
-          x.className = "show";
-          setTimeout(function() {
-            x.className = x.className.replace("show", "");
-          }, 3000);
+            document.getElementById("snackbar").innerHTML = "i";
+            var x = document.getElementById("snackbar");
+            x.className = "show";
+            setTimeout(function() {
+              x.className = x.className.replace("show", "");
+            }, 3000);
+          }
+
+        },
+        complete: function() {
+          $.mobile.loading('hide');
         }
-
-      },
-      complete: function() {
-        $.mobile.loading('hide');
-      }
-    })
-  },
+      })
+    },
 
 
-//login
+  //login
   login: function(mEmail, mPassword) {
     $.ajax({
       url: 'http://amamipro.site/service_login.php',
@@ -134,10 +140,14 @@ register: function(mUsername, mEmail, mPassword) {
         })
       },
       success: function(dataObject) {
-        console.log(dataObject)
-        console.log(dataObject.value["nama"])
-        if (dataObject["nama"] != null) {
+        var obj = JSON.parse(dataObject);
+        Application.gEmail = obj.email;
+        Application.gNama = obj.nama
+        if (obj.email === mEmail) {
+          localStorage['gEmail'] = obj.email;
+          localStorage['gNama'] = obj.nama;
           window.location.replace("./home.html");
+          // $(div).html('<script> setTimeout(function() { window.location = "./home.html"; },10); </script>');
         }
       },
       complete: function() {
@@ -164,5 +174,6 @@ $(document).ready(function() {
   });
 });
 
-document.getElementById("namauser").innerHTML = "Joel";
-document.getElementById("wellcome").innerHTML = " Selamat datang, Joel";
+// document.getElementById("namauser").innerHTML = gNama;
+// document.getElementById("wellcome").innerHTML = " Selamat datang," + gEmail;
+
